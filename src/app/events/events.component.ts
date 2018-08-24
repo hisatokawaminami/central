ca39942d9ea68303440a899549aa4ed1e77c3079
Guest User <Guest@epicodus-50.local>
2018-08-24 16:03:30 -0700

    test mock info

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Show } from '../models/show.model';
 
 @Component({
   selector: 'app-events',
@@ -7,7 +8,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
-
+  shows: Show[] = [
+    new Show('8.20', 'Nirvana', 'Screaming Trees', '8pm', 15),
+    new Show('8.21', 'Wild Arms', 'Hillside', '9pm', 7),
+  ];
   constructor(private router: Router) { }
 
   ngOnInit() {
