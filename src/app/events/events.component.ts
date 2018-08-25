87e4bc4fb65de0c442d3b0122f8eeb4ad1534984
Guest User <Guest@epicodus-50.local>
2018-08-24 17:58:51 -0700

    create service

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Show } from '../models/show.model';
-
+import { ShowService } from '../show.service';
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.css']
+  styleUrls: ['./events.component.css'],
+  providers: [ShowService]
 })
 export class EventsComponent implements OnInit {
-  shows: Show[] = [
-    new Show('8.20', 'Nirvana', 'Screaming Trees', '8pm', 15),
-    new Show('8.21', 'Wild Arms', 'Hillside', '9pm', 7),
-  ];
-  constructor(private router: Router) { }
+  shows: Show[];
+  constructor(private router: Router, private showService: ShowService) { }
 
   ngOnInit() {
   }
